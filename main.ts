173db8561749476726e8ee64deeeb3d6a3aa414d
NatFMe <ing.nataliafrancomedina@gmail.com>
2022-11-15 21:38:01 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,41 +1,92 @@
+function actividad2_tiempo () {
+    music.startMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once)
+    i = 0
+    while (i >= Tiempo * 5) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(100)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.showIcon(IconNames.Heart)
+        basic.pause(5000)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(5000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        i += -1
+    }
+}
+function actividad1_tiempo () {
+    music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
+    i = Tiempo * 5
+    while (i >= 0) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(100)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.showIcon(IconNames.Square)
+        basic.pause(5000)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+        basic.pause(5000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        i += -1
+    }
+}
 input.onButtonPressed(Button.A, function () {
-    Tiempo = Tiempo + 100
+    Tiempo = Tiempo + 1
     basic.showNumber(Tiempo)
     basic.pause(100)
     basic.clearScreen()
 })
+function actividad3_tiempo () {
+    music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
+    i = 0
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(5000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    while (i >= Tiempo * 2) {
+        basic.showIcon(IconNames.Happy)
+        basic.pause(5000)
+        basic.showIcon(IconNames.Happy)
+        basic.pause(5000)
+        basic.showIcon(IconNames.Happy)
+        i += -1
+    }
+}
 input.onButtonPressed(Button.B, function () {
-    Tiempo = Tiempo - 100
+    Tiempo = Tiempo - 1
     basic.showNumber(Tiempo)
     basic.pause(100)
     basic.clearScreen()
 })
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
     if (Tiempo >= 0) {
-        basic.showIcon(IconNames.Yes)
-        music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
-        pins.digitalWritePin(DigitalPin.P2, 1)
-        basic.pause(5000)
-        pins.digitalWritePin(DigitalPin.P2, 0)
-        basic.pause(100)
-        music.startMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Once)
-        basic.showIcon(IconNames.Heart)
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        basic.pause(5000)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        basic.pause(100)
-        music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
+        actividad1_tiempo()
+        actividad2_tiempo()
+        actividad3_tiempo()
         basic.showIcon(IconNames.No)
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        basic.pause(5000)
-        pins.digitalWritePin(DigitalPin.P0, 0)
         basic.pause(100)
         basic.clearScreen()
     }
 })
+let i = 0
 let Tiempo = 0
 basic.showString("Hello!")
-Tiempo = 100
+Tiempo = 1
 pins.digitalWritePin(DigitalPin.P0, 0)
 pins.digitalWritePin(DigitalPin.P1, 0)
 pins.digitalWritePin(DigitalPin.P2, 0)
